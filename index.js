bfcd8c0027701774b45c03a8e5444a04b601a5f3
Andrew Masih <armasih@outlook.com>
2021-06-01 21:40:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/* comparators are JS operators, they compare 2 values and return the boolean result TRUE or FALSE, put 1 value on the leftside, the other on the rightside and add a comparator between them */
+
+/* comparators: 
+< is less than, 
+< = is less than or equal to, 
+== is equal to, 
+> = is greater than or equal to, 
+> is greater than. */
+
+let a = 10;
+let b = 20;
+
+console.log(a<b); // , a is less than b
+console.log(a<=b); // a is lass than or equal to b  
+console.log(a==b); // a is equal to b 
+console.log(a>=b); // a is greater than or equal to b 
+console.log(a>b); // a is greater than b 
